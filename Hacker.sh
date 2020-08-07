b75aeca2bb154ec11660e5b8079eeec3df60512b
WahyuGaming-ID <69355106+WahyuGaming-ID@users.noreply.github.com>
2020-08-07 21:34:42 +0700

    Add files via upload

---
+++ Hacker.sh
@@ -0,0 +1,203 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay Sc Hacker
+sleep 1
+echo
+echo $yellow "^_^Welcome To Script Hacker.Sh^_^"
+echo $blue "+_+Login Script Terlebih Dahulu+_+"
+echo
+read -p ":======>>>>" Wahyu
+read -p ":======>>>>" Gaming
+sleep 1
+echo
+echo $green "Loading... 20%"
+sleep 1
+echo $yellow"Loading.. 75%"
+sleep 2
+echo $blue"Loading. 100"
+sleep 4
+toilet -f big -F gay Done
+sleep 2
+clear
+toilet -f big -F gay Sc Hacker
+echo
+echo $cyan"╔════════════════════════════════════════════════╗"
+echo $cyan"║             Script Hacker Ind                  ║"
+echo $cyan"╠════╦══════════════════════════════════════╦════╣"
+echo $cyan"║  1 ║ Hack Facebook New                    ║  1 ║"
+echo $cyan"║  2 ║ Hack Facebook Dark Setan             ║  2 ║"
+echo $cyan"║  3 ║ Hack Facebook No Login Facebook      ║  3 ║"
+echo $cyan"║  4 ║ Hack Cctv For Login                    GNU nano 4.9.3                      Hacker.sh                       Modified
+sleep 1
+echo
+echo $green "Loading... 20%"
+sleep 1
+echo $yellow"Loading.. 75%"
+sleep 2
+echo $blue"Loading. 100"
+sleep 4
+toilet -f big -F gay Done
+sleep 2
+clear
+toilet -f big -F gay Sc Hacker
+echo
+echo $cyan"╔════════════════════════════════════════════════╗"
+echo $cyan"║             Script Hacker Ind                  ║"
+echo $cyan"╠════╦══════════════════════════════════════╦════╣"
+echo $cyan"║  1 ║ Hack Facebook New                    ║  1 ║"
+echo $cyan"║  2 ║ Hack Facebook Dark Setan             ║  2 ║"
+echo $cyan"║  3 ║ Hack Facebook No Login Facebook      ║  3 ║"
+echo $cyan"║  4 ║ Hack Cctv For Login Web              ║  4 ║"
+echo $cyan"║  5 ║ Hack Situs Web SqoulMap              ║  5 ║"
+echo $cyan"║  6 ║ Hack Whatssap Pacar/Teman/Orang      ║  6 ║"
+echo $cyan"║  7 ║ Ganti Background Termux/Linux        ║  7 ║"
+echo $cyan"║  8 ║ Edit Aplikasi                        ║  8 ║"
+echo $cyan"║  9 ║ Kirim Virus Trojan Dengan No Hp      ║  9 ║"
+echo $cyan"║ 10 ║ Kirim Virus Ke Web                   ║ 10 ║"
+echo $cyan"║ 11 ║ Recode Script Hacker Ind             ║ 11 ║"
+echo $cyan"║ 12 ║ Hack Wifi(Root Acces)                ║ 12 ║"
+echo $cyan"║ 13 ║ Merusak Hp Orang/Musuh(Root Acces    ║ 13 ║"
+echo $cyan"║ 14 ║ Membuat Nama Kosong Facebook/Game    ║ 14 ║"
+echo $cyan"║ 15 ║ Hack Diamond All Game(Root Acces     ║ 15 ║"
+echo $cyan"║ 16 ║ Beli Script Vip Hacker(Facebook)     ║ 16 ║"
+echo $cyan"║ 17 ║ Beli Diamond Ilegal/Legal Aman       ║ 17 ║"
+echo $cyan"║ 18 ║ Recode Bilang Dulu(Facebook)         ║ 18 ║"
+echo $cyan"║ 19 ║ Cheat Free Fire Terbaru 2020         ║ 19 ║"
+echo $cyan"║ 20 ║ Keluar Disini Goblog                 ║ 20 ║"
+echo $cyan"╚════╩══════════════════════════════════════╩════╝"
+echo $green
+read -p ":═════════►" Anjay
+#sampai sini paham
+if [ $Anjay = 1 ] || [ $Anjay = 1 ]
+then
+git clone https://github.com/TheMagizz/DarkPremium
+cd DarkPremium
+python2 DarkFB.py
+fi
+#Sampai Sini Paham
+if [ $Anjay = 2 ] || [ $Anjay = 2 ]
+then
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+python2 SETAN.py
+fi
+#sampai sini paham
+if [ $Anjay = 3 ] || [ $Anjay = 3 ]
+then
+git clone https://github.com/ofekp/Sensei
+cd Sensei
+python2 Sensei.py
+fi
+#Sampai Sini Paham
+if [ $Anjay = 4 ] || [ $Anjay = 4 ]
+then
+git clone https://github.com/AngelSecurityTeam/Cam-Hackers
+cd Cam-Hackers
+python2 cam-hanckers.py
+fi
+#sampai sini paham
+if [ $Anjay = 5 ] || [ $Anjay = 5 ]
+then
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+python2 sqlmap.py
+fi
+#Sampai Sini Paham
+if [ $Anjay = 6 ] || [ $Anjay = 6 ]
+then
+git clone https://github.com/raptored01/whatsapp_hack
+cd whatsapp_hack
+python2 grabber.py
+fi
+#sampai sini paham
+if [ $Anjay = 7 ] || [ $Anjay = 7 ]
+then
+git clone https://gist.github.com/jsmanifest/b57c41c5f5ea8b76f4007805a0a6858f
+cd b57c41c5f5ea8b76f4007805a0a6858f
+background.js
+fi
+#Sampai Sini Paham
+if [ $Anjay = 8 ] || [ $Anjay = 8 ]
+echo $red
+echo "Enggak Ada Pego"
+sh Hacker.sh
+fi
+#sampai sini paham
+if [ $Anjay = 9 ] || [ $Anjay = 9 ]
+then
+git clone https://github.com/R133F/Trojans
+cd Trojans
+python2 trojans.py
+fi
+#Sampai Sini Paham
+if [ $Anjay = 10 ] || [ $Anjay = 10 ]
+then
+git clone http://github.com/cyweb/hammer
+cd hammer
+python2 hyammer.py
+fi
+#sampai sini paham
+if [ $Anjay = 11 ] || [ $Anjay = 11 ]
+then
+nano Hacker.sh
+fi
+#Sampai Sini Paham
+if [ $Anjay = 12 ] || [ $Anjay = 12 ]
+then
+echo $red "Root Terlebih Dahulu Anjing"
+fi
+#sampai sini paham
+if [ $Anjay = 13 ] || [ $Anjay = 13 ]
+then
+echo $red "Root Terlebih Dahulu Anjing"
+fi
+#Sampai Sini Paham
+if [ $Anjay = 14 ] || [ $Anjay = 14 ]
+then
+echo $green"Tanya Di Facebook Wahyu Khancill"
+echo "Link:https://www.facebook.com/bangk.wahyucah"
+fi
+#sampai sini paham
+if [ $Anjay = 15 ] || [ $Anjay = 15 ]
+then
+exit
+clear
+fi
+#sampai sini paham
+if [ $Anjay = 16 ] || [ $Anjay = 16 ]
+then
+echo $green"Tanya Di Facebook Wahyu Khancill"
+echo "Link:https://www.facebook.com/bangk.wahyucah"
+fi
+#Sampai Sini Paham
+if [ $Anjay = 17 ] || [ $Anjay = 17 ]
+then
+echo $green"Tanya Di Facebook Wahyu Khancill"
+echo "Link:https://www.facebook.com/bangk.wahyucah"
+fi
+#Sampai Sini Paham
+if [ $Anjay = 18 ] || [ $Anjay = 18 ]
+then
+echo $green"Tanya Di Facebook Wahyu Khancill"
+echo "Link:https://www.facebook.com/bangk.wahyucah"
+fi
+#sampai sini paham
+if [ $Anjay = 19 ] || [ $Anjay = 19 ]
+then
+echo $green"Tanya Di Facebook Wahyu Khancill"
+echo "Link:https://www.facebook.com/bangk.wahyucah"
+fi
+#Sampai Sini Paham
+if [ $Anjay = 20 ] || [ $Anjay = 20 ]
+then
+exit
+clear
+fi
+#Sampai Sini Paham
